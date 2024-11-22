cc617014b497213b228c126551c2d1c63c984fcc
mustbeneru <neilcarlo.zapanta.d@gmail.com>
2024-11-23 03:14:14 +0800

    asdoipasd

--- routes/events.js
+++ routes/events.js
@@ -6,10 +6,9 @@ require('dotenv').config()
 
 // Add a new event
 router.post('/', (req, res) => {
-	const { title, date, location, organizer, category } = req.body
-	const query =
-		'INSERT INTO events (title, date, location, organizer, category) VALUES (?, ?, ?, ?, ?)'
-	db.query(query, [title, date, location, organizer, category], (err, result) => {
+	const { title, description, date, time, location, organizer, category, recurrence } = req.body
+	const query = 'INSERT INTO events (title, description, date, time, location, organizer, category, recurrence) VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+	db.query(query, [title, description, date, time, location, organizer, category, recurrence], (err, result) => {
 		if (err) return res.status(500).json({ error: err.message })
 		res.status(201).json({
 			id: result.insertId,
@@ -30,10 +29,9 @@ router.get('/', (req, res) => {
 // Update an event
 router.put('/:id', (req, res) => {
 	const eventId = req.params.id
-	const { title, date, location, organizer, category } = req.body
-	const query =
-		'UPDATE events SET title = ?, date = ?, location = ?, organizer = ?, category = ? WHERE id = ?'
-	db.query(query, [title, date, location, organizer, category, eventId], (err, result) => {
+	const { title, description, date, time, location, organizer, category, recurrence } = req.body
+	const query = 'UPDATE events SET title = ?, description = ?, date = ?, time = ?, location = ?, organizer = ?, category = ?, recurrence = ? WHERE id = ?'
+	db.query(query, [title, description, date, time, location, organizer, category, recurrence, eventId], (err, result) => {
 		if (err) return res.status(500).json({ error: err.message })
 		if (result.affectedRows === 0) return res.status(404).json({ message: 'Event not found' })
 		res.status(200).json({ message: 'Event updated successfully' })
